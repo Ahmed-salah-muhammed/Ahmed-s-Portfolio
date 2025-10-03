d0b359bb55d617d84bce11f9bb795308e2a72898
Ahmed Salah Abd El-bari <142721848+Ahmed-salah-muhammed@users.noreply.github.com>
2025-10-03 19:59:05 +0300

    final result

--- app.js
+++ app.js
@@ -1,7 +1,50 @@
 const hamburger = document.querySelector("#hamburger")
 const menu = document.querySelector("#menu")
+const body = document.querySelector("#body")
+const hlink = document.querySelectorAll("#hlink")
+const chatBot = document.getElementById('chat-bot');
+const chatWindow = document.getElementById('chat-window');
+const chatSend = document.getElementById('chat-send');
+const chatInput = document.getElementById('chat-input');
+const chatMessages = document.getElementById('chat-messages');
 
 hamburger.addEventListener("click", ()=>{
     menu.classList.toggle("hidden")
     hamburger.classList.toggle("bg-white")
 })
+
+hlink.forEach(link => {
+    link.addEventListener("click", ()=>{
+        menu.classList.add("hidden")
+        hamburger.classList.remove("bg-white")
+    })
+})
+
+hamburger.addEventListener("mouseenter", () => {
+menu.classList.remove("hidden");
+hamburger.classList.add("bg-white");
+});
+
+hamburger.addEventListener("mouseleave", () => {
+menu.classList.add("hidden");
+hamburger.classList.remove("bg-white");
+});
+
+menu.addEventListener("mouseenter", () => {
+menu.classList.remove("hidden");
+hamburger.classList.add("bg-white");
+});
+
+menu.addEventListener("mouseleave", () => {
+menu.classList.add("hidden");
+hamburger.classList.remove("bg-white");
+});
+
+body.addEventListener("click", (e)=>{
+    if(e.target.id != "menu" && e.target.id != "hamburger" && e.target.id != "line"){
+        menu.classList.add("hidden")
+        hamburger.classList.remove("bg-white")
+    }
+})
+
+
